b6db0098bc35992e9deb355559e56938ddbffbdb
junior <junior.kaningini@unikin.ac.cd>
2020-08-15 00:32:38 +0200

    module4 solution

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -19,7 +19,6 @@ var speakWord = "Good Bye";
 function speak(name) {
   console.log(speakWord + " " + name);
 }
-
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
 // xxxx.xxxx = byeSpeaker;
